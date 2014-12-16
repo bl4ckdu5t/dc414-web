35a311e7fd0d1e8897a5e0ff6a2d0e15be86d2c5
bl4ckdu5t <joerexko@gmail.com>
2014-12-16 05:46:52 -0600

    Added dynamism to the remote IP display

--- js/script.js
+++ js/script.js
@@ -4,5 +4,10 @@
 * @since 12-05-2014
 */
 $(document).ready(function(){
-	
+	/* Fetch IP address from third-party service */
+	$.getJSON('http://api.ipify.org?format=jsonp&callback=?').done(function(response){
+		$('.ip-addr').text(response.ip);
+	}).fail(function(){
+		$('.ip-addr').text('0.0.0.0');
+	});
 });
